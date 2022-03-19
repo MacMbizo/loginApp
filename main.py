c7861f150795bbe9e9291b062bd423ee457ae3ca
Mac Mbizo <macmbizo@gmail.com>
2022-03-19 12:01:40 +0200

    Created LoginApp class

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog,QApplication, QMessageBox
+from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox
 from PyQt5.uic import loadUi
 import mysql.connector as connector
 
+
 class LoginApp(QDialog):
     def __init__(self):
-        super(logidApp)
-
-
-
-
+        super(LoginApp, self).__init__()
+        loadUi("login-form.ui", self)
