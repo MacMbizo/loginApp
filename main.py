ef145edea7054087f345ed6d3994d1e80fc228ae
Mac Mbizo <macmbizo@gmail.com>
2022-03-19 09:41:12 +0200

    Creating main file

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog,QApplication, QMessageBox
+from PyQt5.uic import loadUi
+import mysql.connector as connector
+
+class LoginApp(QDialog):
+    def __init__(self):
+        super(logidApp)
+
+
+
+
